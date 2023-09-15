a08f9b083e242ad4b2b14a6c74e1a15fd6ba7160
YG-Calo <3164522206@qq.com>
2023-09-15 17:32:56 +0800

    添加本地事件总线拓展方法

--- Y.EventBus/EventBusExtensions.cs
+++ Y.EventBus/EventBusExtensions.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
 using Y.Module.Extensions;
 
 namespace Y.EventBus
 {
     public  static class EventBusExtensions
     {
-        public static IServiceCollection AddEventBusWithHandeler(this IServiceCollection services,Action<EventHandlerContainer> action)
+        //添加事件总线并且添加订阅
+        public static IServiceCollection AddEventBusAndSubcrise(this IServiceCollection services,Action<EventHandlerContainer> action)
         {
             services.ChcekNull();
 
@@ -20,13 +22,38 @@ namespace Y.EventBus
             return services;
         }
 
+        //开始消费
         public static async Task StartConsumer(this IServiceProvider serviceProvider,Func<IEventHandlerManager,Task> func)
         {
             var scope = serviceProvider.CreateAsyncScope(); 
 
-            var eventhandlerManager = scope.ServiceProvider.GetService<IEventHandlerManager>(); 
+            var eventhandlerManager = scope.ServiceProvider.GetService<IEventHandlerManager>();
+
+            await eventhandlerManager.Subcrice();
 
             await func(eventhandlerManager);
+        }
+
+        //添加本地事件总线
+        public static IServiceCollection AddEventBus(this IServiceCollection services)
+        {
+            services.ChcekNull();
+
+            services.AddSingleton<IEventHandlerManager, EventHandlerManager>();
+
+            services.AddSingleton<ILocalEventBus, LocalEventBus>();
+
+            return services;
+        }
+
+        //添加订阅
+        public static IServiceCollection AddSubcrise(this IServiceCollection services, Action<EventHandlerContainer> action)
+        {
+            EventHandlerContainer eventHandlerContainer = new EventHandlerContainer(services);
+
+            action.Invoke(eventHandlerContainer);
+
+            return services;
         }
     }
 }
